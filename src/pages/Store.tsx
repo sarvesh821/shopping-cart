21c1a348feee1271b9bc942bc60efea0debb4624
Sarvesh <sarveshgoyal4152@gmail.com>
2024-06-28 15:01:52 +0530

    last changes

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,18 +1,18 @@
-import { Col, Row } from 'react-bootstrap'
-import storeItems from '../data/items.json'
+import { Col, Row } from 'react-bootstrap';
+import storeItems from '../data/items.json';
+import { StoreItem } from '../components/StoreItem'; // Ensure the path is correct
 
-import { StoreItem } from '../components/StoreItem'
-
-
-export function Store(){
-   return (
+export function Store() {
+  return (
     <>
-    <h1>Store</h1>
-    <Row md={2} xs={1} lg={3} className='g-3'>
-        {storeItems.map((item=>(
-            <Col><StoreItem {...item}/></Col>
-        )))}
-    </Row>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map(item => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
     </>
-   )
+  );
 }
